0edeb66b52f46e4b6d0c574042721f8e5b3fb32f
Daniel Hartwich <daniel.hartwich@xing.com>
2015-11-05 16:30:50 +0100

    Remove Nearby Sample

--- manualbluetoothlescanner/src/main/java/com/devfest/manualbluetoothparsing/MainActivity.java
+++ manualbluetoothlescanner/src/main/java/com/devfest/manualbluetoothparsing/MainActivity.java
@@ -8,8 +8,10 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.ParcelUuid;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.math.BigInteger;
@@ -18,6 +20,10 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private TextView distanceBeacon;
+    private TextView temperatureBeacon;
+    private TextView temperatureTrend;
+    private RelativeLayout mainLayout;
+    private double lastMeasuredTemperature = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         distanceBeacon = (TextView) findViewById(R.id.distance_beacon);
+        temperatureBeacon = (TextView) findViewById(R.id.beacon_temperature);
+        temperatureTrend = (TextView) findViewById(R.id.temperature_trend);
+        mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
+
         BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-
         BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
 
         bluetoothLeScanner.startScan(new ScanCallback() {
@@ -43,36 +52,67 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                //Get the FrameType to check which data is available
                 byte frameType = data[0];
-                if (frameType != 0x00) {
-                    return;
+                //Log.d("FrameType: ", frameType + "");
+                if (frameType == 0x00) {
+
+                    String namespace = new BigInteger(1, Arrays.copyOfRange(data, 2, 12)).toString(16);
+                    String instance = new BigInteger(1, Arrays.copyOfRange(data, 12, 18)).toString(16);
+
+                    //Log.d("namespace instance", namespace + " " + instance);
+                    //My Beacon info
+                    if ((namespace + instance).equals("edd1ebeac04e5defa017" + "d7b4fb5665ad")) {
+
+                        //Signal Strength
+                        int rssi = result.getRssi();
+
+                        //Signal strength emitted by the beacon in dBm at 0 meters distance
+                        int txPower = data[1];
+
+                        //pathLoss = (txPower at 0m - rssi)
+                        double distance = Math.pow(10, ((txPower - rssi) - 41) / 20.0);
+
+                        //because rssi is unstable, usually only proximity zones are used:
+                        // immediate (very close to the beacon)
+                        // near (about 1-3m away from the beacon)
+                        // far (further away or the signal is fluctuating too much to make a better calculation)
+                        // unknown
+                        Log.i("distance", String.format("%.2fm", distance));
+
+                        distanceBeacon.setText(String.format("%.2fm", distance));
+                    }
+                } else if (frameType == 0x20) {
+                    //Get the temperature for my beacon
+                    if (result.getDevice().getAddress().equals("D7:B4:FB:56:65:AD")) {
+                        double temperature = new BigInteger(Arrays.copyOfRange(data, 4, 6)).intValue() / 256.00;
+
+                        //Calculate the trend of the temperature
+                        if (lastMeasuredTemperature < temperature) {
+                            temperatureTrend.setText(R.string.trend_warmer);
+                        } else if (lastMeasuredTemperature > temperature) {
+                            temperatureTrend.setText(R.string.trend_colder);
+                        } else {
+                            temperatureTrend.setText(R.string.trend_no_difference);
+                        }
+                        lastMeasuredTemperature = temperature;
+
+                        Log.d("Temperature", String.format("%.2f°C", temperature));
+                        temperatureBeacon.setText(String.format("%.2f°C", temperature));
+
+                        //Set the background color according to the temperature
+                        if (temperature <= 18.5) {
+                            mainLayout.setBackgroundColor(
+                                    ContextCompat.getColor(MainActivity.this, android.R.color.holo_blue_light));
+                        } else if (temperature >= 19.5) {
+                            mainLayout.setBackgroundColor(
+                                    ContextCompat.getColor(MainActivity.this, android.R.color.holo_red_light));
+                        } else {
+                            mainLayout.setBackgroundColor(
+                                    ContextCompat.getColor(MainActivity.this, android.R.color.holo_green_light));
+                        }
+                    }
                 }
-
-                String namespace = new BigInteger(1, Arrays.copyOfRange(data, 2, 12)).toString(16);
-                String instance = new BigInteger(1, Arrays.copyOfRange(data, 12, 18)).toString(16);
-
-                //My Beacon info
-                if (!(namespace + instance).equals("edd1ebeac04e5defa017" + "d7b4fb5665ad")) {
-                    return;
-                }
-
-                //Signal Strength
-                int rssi = result.getRssi();
-
-                //Signal strength emitted by the beacon in dBm at 0 meters distance
-                int txPower = data[1];
-
-                //pathLoss = (txPower at 0m - rssi)
-                double distance = Math.pow(10, ((txPower - rssi) - 41) / 20.0);
-
-                //because rssi is unstable, usually only proximity zones are used:
-                // immediate (very close to the beacon)
-                // near (about 1-3m away from the beacon)
-                // far (further away or the signal is fluctuating too much to make a better calculation)
-                // unknown
-                Log.i("distance", String.format("%.2fm", distance));
-
-                distanceBeacon.setText(String.format("%.2fm", distance));
             }
         });
     }
